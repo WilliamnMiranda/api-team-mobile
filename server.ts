122716547f512a3c55c3be7e1dccc6741e51476f
william nascimento <williamnnmiranda@gmail.com>
2023-04-25 12:54:50 -0300

    feat: :sparkles: created the connection with the bank

--- server.ts
+++ server.ts
@@ -1,7 +1,23 @@
+import mongoose from "mongoose";
+
 const express = require("express");
 const app = express();
-
 const PORT = 8001;
+
+mongoose
+	.connect(process.env.DATABASE, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log("conectado ao banco");
+		return "conectado ao banco";
+	})
+	.catch((e) => {
+		console.log("error");
+		console.log(e);
+		return e;
+	});
 
 app.listen(PORT, () => {
 	console.log(`Example app listening on port ${PORT}`);
