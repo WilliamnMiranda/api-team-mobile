523383c90936990a05fbb9f31c62b1d4d4a9f165
william nascimento <williamnnmiranda@gmail.com>
2023-04-25 13:01:04 -0300

    feat: :sparkles: added bank connection

--- server.ts
+++ server.ts
@@ -1,23 +1,18 @@
-import mongoose from "mongoose";
-
+import mongoose, { ConnectOptions } from "mongoose";
 const express = require("express");
 const app = express();
-const PORT = 8001;
-
+const PORT = process.env.PORT || 8081;
+require("dotenv").config();
 mongoose
-	.connect(process.env.DATABASE, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	})
-	.then(() => {
-		console.log("conectado ao banco");
-		return "conectado ao banco";
-	})
-	.catch((e) => {
-		console.log("error");
-		console.log(e);
-		return e;
-	});
+	.connect(
+		process.env.DATABASE as string,
+		{
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		} as ConnectOptions,
+	)
+	.then(() => console.log("conectado ao banco"))
+	.catch((e) => console.log("error"));
 
 app.listen(PORT, () => {
 	console.log(`Example app listening on port ${PORT}`);
