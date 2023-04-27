07f8c8ea06c1d1a7fda5201bb5d226edbdd1480f
william nascimento <williamnnmiranda@gmail.com>
2023-04-27 12:00:05 -0300

    feat: :sparkles: added token creation when creating account

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,13 +1,20 @@
 import UserModel from "../models/UserModel";
+import jwt from "jsonwebtoken";
 import { Request, Response } from "express";
 export const create = async (req: Request, res: Response) => {
 	const { name, email, password, cpf } = req.body;
+	if (!name || !email || !cpf || !password)
+		res.status(406).json({ error: "Todos os campos sao obrigatorios" });
 	const user = new UserModel({ name, email, password, cpf });
 	try {
 		await user.save();
+		const token = jwt.sign({ email }, process.env.SECRET as string, {
+			expiresIn: 300000000,
+		});
+		const userLogged = { name, email, token };
 		res.status(200).json(user);
 	} catch (e) {
 		console.log(e);
-		res.status(422).json({ error: "Email ou CPF ja cadastrados" });
+		res.status(422).json({ error: "Email ou CPF ja cadastrado" });
 	}
 };
