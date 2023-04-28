47736a374bce3820d1a63a8af947c4c8ff80900a
william nascimento <williamnnmiranda@gmail.com>
2023-04-28 14:37:59 -0300

    feat: :sparkles: added authentication middleware

---
+++ src/middlewares/Authenticate.ts
@@ -0,0 +1,31 @@
+const User = require("../models/UserModel");
+import { Request, Response, NextFunction } from "express";
+import { IUser } from "../interfaces/UserInterface";
+const jwt = require("jsonwebtoken");
+
+interface ResquestAuthenticateUser extends Request {
+	user?: IUser;
+}
+
+const AuthenticateUser = async (
+	req: ResquestAuthenticateUser,
+	res: Response,
+	next: NextFunction,
+) => {
+	const { token } = req.body;
+	if (!token) return res.status(500).json("no token provider");
+	jwt.verify(
+		token,
+		process.env.SECRET,
+		async function (err: any, decoded: any) {
+			if (err)
+				return res
+					.status(500)
+					.json({ message: "Failed to authenticate token." });
+			req.user = await User.findOne({ email: decoded.email });
+			next();
+		},
+	);
+};
+
+export default AuthenticateUser;
--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 
 import { create, login } from "../controllers/UserController";
+import AuthenticateUser from "../middlewares/Authenticate";
 
 const userRouter = Router();
 
-userRouter.post("/create", create);
+userRouter.post("/create", AuthenticateUser, create);
 userRouter.post("/login", login);
 
 export default userRouter;
