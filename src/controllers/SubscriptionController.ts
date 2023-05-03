e72e09c3f4562494116a56dfe9f54427f5e0a86d
william nascimento <williamnnmiranda@gmail.com>
2023-05-03 18:29:33 -0300

    feat: :sparkles: added middleware to change the status of a subscription

--- src/controllers/SubscriptionController.ts
+++ src/controllers/SubscriptionController.ts
@@ -1,7 +1,9 @@
+import { Types } from "mongoose";
 import { RequestProjectWithAuthentication } from "../interfaces/ProjectInterface";
 import ProjectModel from "../models/ProjectModel";
 import SubscriptionModel from "../models/SubscriptionModel";
-import { Response } from "express";
+import { Response, Request } from "express";
+
 export const requestSubscription = async (
 	req: RequestProjectWithAuthentication,
 	res: Response,
@@ -34,4 +36,46 @@ export const requestSubscription = async (
 	await newSubscription.save();
 
 	res.status(201).json({ message: "Inscricao enviada ao lider do projeto" });
+};
+
+export const updateSubscription = async (
+	req: RequestProjectWithAuthentication,
+	res: Response,
+) => {
+	const { subscriptionId, statusSubscription } = req.body;
+
+	if (!Types.ObjectId.isValid(subscriptionId)) {
+		return res.status(400).json({ message: "ID de assinatura inválido." });
+	}
+
+	try {
+		const subscription = await SubscriptionModel.findOneAndUpdate(
+			{
+				_id: subscriptionId,
+			},
+			{ status: statusSubscription },
+			{ new: true },
+		);
+
+		if (!subscription) {
+			return res.status(404).json({ message: "Assinatura não encontrada." });
+		}
+
+		const project = await ProjectModel.findById(subscription.project);
+
+		if (project?.owner.toString() !== req.user?._id.toString())
+			return res
+				.status(401)
+				.json("Apenas o lider do projeto pode alterar o status");
+
+		subscription.save();
+		res.status(200).json({
+			message: `Assinatura alterada para ${statusSubscription}.`,
+			data: subscription,
+		});
+	} catch (e) {
+		res
+			.status(500)
+			.json({ message: "Ocorreu um erro ao modificar a assinatura." });
+	}
 };
--- src/routes/SubscriptionRoutes.ts
+++ src/routes/SubscriptionRoutes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 
-import { requestSubscription } from "../controllers/SubscriptionController";
+import {
+	requestSubscription,
+	updateSubscription,
+} from "../controllers/SubscriptionController";
 import ValidateToken from "../middlewares/ValidateToken";
 
 const subscriptionRouter = Router();
 
 subscriptionRouter.post("/create", ValidateToken, requestSubscription);
+subscriptionRouter.put("/change", ValidateToken, updateSubscription);
 
 export default subscriptionRouter;
