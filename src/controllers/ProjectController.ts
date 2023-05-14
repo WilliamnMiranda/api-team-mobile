b4d765149aa3a38898d50cd21f755f8aa6e2a173
william nascimento <williamnnmiranda@gmail.com>
2023-05-14 11:24:12 -0300

    feat: :sparkles: added new fields in project model

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -4,162 +4,164 @@ import { RequestProjectWithAuthentication } from "../interfaces/ProjectInterface
 import UserModel from "../models/UserModel";
 
 export const create = async (
-	req: RequestProjectWithAuthentication,
-	res: Response,
+  req: RequestProjectWithAuthentication,
+  res: Response
 ) => {
-	const { name, technologies, description } = req.body;
-	if (!name) res.status(422).json("O nome do projeto e obrigatorio");
-	if (!description)
-		res.status(422).json("A descricao do projeto e obrigatoria");
-	if (!technologies)
-		res.status(422).json("As tecnologias do projeto sao obrigatorias");
+  const { name, technologies, description, coreTechnology, level } = req.body;
+  if (!name) res.status(422).json("O nome do projeto e obrigatorio");
+  if (!description)
+    res.status(422).json("A descricao do projeto e obrigatoria");
+  if (!technologies)
+    res.status(422).json("As tecnologias do projeto sao obrigatorias");
 
-	try {
-		const user = await UserModel.findById(req.user?._id);
-		if (!user) {
-			res.status(404).json("Usuario nao encontrado");
-			return;
-		}
+  try {
+    const user = await UserModel.findById(req.user?._id);
+    if (!user) {
+      res.status(404).json("Usuario nao encontrado");
+      return;
+    }
 
-		const project = new ProjectModel({
-			name,
-			technologies,
-			description,
-			owner: user._id,
-		});
+    const project = new ProjectModel({
+      name,
+      technologies,
+      description,
+      coreTechnology,
+      level,
+      owner: user._id,
+    });
 
-		await project.save();
+    await project.save();
 
-		user.projects.push(project._id);
-		await user.save();
+    user.projects.push(project._id);
+    await user.save();
 
-		res.status(200).json(project);
-	} catch (e) {
-		res.status(500).json("Error no servidor");
-	}
+    res.status(200).json(project);
+  } catch (e) {
+    res.status(500).json("Error no servidor");
+  }
 };
 
 export const deleteProject = async (
-	req: RequestProjectWithAuthentication,
-	res: Response,
+  req: RequestProjectWithAuthentication,
+  res: Response
 ) => {
-	const { id } = req.params;
+  const { id } = req.params;
 
-	const project = await ProjectModel.findById(id);
+  const project = await ProjectModel.findById(id);
 
-	if (project) {
-		const userId = req.user?._id?.toString();
+  if (project) {
+    const userId = req.user?._id?.toString();
 
-		// Remove o ID do projeto da lista de projetos do usuário
-		const user = await UserModel.findById(userId);
-		if (!user) {
-			return res.status(404).json({
-				response: false,
-				message: "Não foi possível localizar o usuário",
-			});
-		}
-		user.projects = user.projects.filter((p) => p.toString() !== id);
-		await user.save();
+    // Remove o ID do projeto da lista de projetos do usuário
+    const user = await UserModel.findById(userId);
+    if (!user) {
+      return res.status(404).json({
+        response: false,
+        message: "Não foi possível localizar o usuário",
+      });
+    }
+    user.projects = user.projects.filter((p) => p.toString() !== id);
+    await user.save();
 
-		if (project.owner.toString() !== userId) {
-			return res.status(401).json({
-				response: false,
-				message: "Você não tem autorização para deletar este projeto",
-			});
-		}
+    if (project.owner.toString() !== userId) {
+      return res.status(401).json({
+        response: false,
+        message: "Você não tem autorização para deletar este projeto",
+      });
+    }
 
-		await ProjectModel.findByIdAndDelete(id);
+    await ProjectModel.findByIdAndDelete(id);
 
-		return res.status(200).json({
-			response: true,
-			message: "Projeto deletado com sucesso",
-		});
-	} else {
-		return res.status(404).json({
-			response: false,
-			message: "Não foi possível localizar o projeto",
-		});
-	}
+    return res.status(200).json({
+      response: true,
+      message: "Projeto deletado com sucesso",
+    });
+  } else {
+    return res.status(404).json({
+      response: false,
+      message: "Não foi possível localizar o projeto",
+    });
+  }
 };
 
 export const getAllUserProjects = async (
-	req: RequestProjectWithAuthentication,
-	res: Response,
+  req: RequestProjectWithAuthentication,
+  res: Response
 ) => {
-	const { id } = req.params;
-	const projects = await ProjectModel.find({
-		owner: id,
-	});
+  const { id } = req.params;
+  const projects = await ProjectModel.find({
+    owner: id,
+  });
 
-	if (!projects)
-		return res.status(404).json({
-			response: false,
-			message: "O usuario nao tem nenhum projeto criado",
-		});
+  if (!projects)
+    return res.status(404).json({
+      response: false,
+      message: "O usuario nao tem nenhum projeto criado",
+    });
 
-	res.status(200).json(projects);
+  res.status(200).json(projects);
 };
 
 export const getOneUserProject = async (
-	req: RequestProjectWithAuthentication,
-	res: Response,
+  req: RequestProjectWithAuthentication,
+  res: Response
 ) => {
-	const { id } = req.params;
-	const project = await ProjectModel.findById(id);
+  const { id } = req.params;
+  const project = await ProjectModel.findById(id);
 
-	if (!project)
-		return res.status(404).json({
-			response: false,
-			message: "Nenhum projeto encontrado",
-		});
+  if (!project)
+    return res.status(404).json({
+      response: false,
+      message: "Nenhum projeto encontrado",
+    });
 
-	return res.status(200).json(project);
+  return res.status(200).json(project);
 };
 
 export const signUpForTheProject = async (
-	req: RequestProjectWithAuthentication,
-	res: Response,
+  req: RequestProjectWithAuthentication,
+  res: Response
 ) => {
-	const { projectId } = req.body;
-	const project = await ProjectModel.findById(projectId);
-	if (!project) return res.status(404).json("Projeto nao encontrado");
+  const { projectId } = req.body;
+  const project = await ProjectModel.findById(projectId);
+  if (!project) return res.status(404).json("Projeto nao encontrado");
 
-	// Check if user is already subscribed
-	const isAlreadySubscribed = project.subscriptions.includes(req.user?._id);
-	if (isAlreadySubscribed) {
-		return res.status(400).json("Usuario ja esta inscrito neste projeto");
-	}
+  // Check if user is already subscribed
+  const isAlreadySubscribed = project.subscriptions.includes(req.user?._id);
+  if (isAlreadySubscribed) {
+    return res.status(400).json("Usuario ja esta inscrito neste projeto");
+  }
 
-	try {
-		project.subscriptions.push(req.user?._id);
-		await project.save();
-		res.status(200).json(project);
-	} catch (e) {
-		res.status(500).json({
-			response: false,
-			message: "Error interno no servidor",
-		});
-	}
+  try {
+    project.subscriptions.push(req.user?._id);
+    await project.save();
+    res.status(200).json(project);
+  } catch (e) {
+    res.status(500).json({
+      response: false,
+      message: "Error interno no servidor",
+    });
+  }
 };
 
 export const acceptOrRejectOrders = async (
-	req: RequestProjectWithAuthentication,
-	res: Response,
+  req: RequestProjectWithAuthentication,
+  res: Response
 ) => {
-	const { userId } = req.body;
+  const { userId } = req.body;
 };
 
 export const getRelevanteProjects = async (
-	req: RequestProjectWithAuthentication,
-	res: Response,
+  req: RequestProjectWithAuthentication,
+  res: Response
 ) => {
-	const user = await UserModel.findById(req.body._id); // busca o usuário pelo id
+  const user = await UserModel.findById(req.body._id); // busca o usuário pelo id
 
-	const interests = user!.interests; // obtém os interesses do usuário
+  const interests = user!.interests; // obtém os interesses do usuário
 
-	const projects = await ProjectModel.aggregate([
-		{ $match: { technologies: { $in: interests } } },
-		{ $sort: { views: -1 } },
-		{ $limit: 10 },
-	]);
+  const projects = await ProjectModel.aggregate([
+    { $match: { technologies: { $in: interests } } },
+    { $sort: { views: -1 } },
+    { $limit: 10 },
+  ]);
 };
--- src/models/ProjectModel.ts
+++ src/models/ProjectModel.ts
@@ -2,40 +2,50 @@ import mongoose from "mongoose";
 import { IUser } from "../interfaces/UserInterface";
 
 interface IProject extends mongoose.Document {
-	name: String;
-	owner: String;
-	team: String[];
-	technologies: String[];
-	likes: Number;
-	description: String;
-	participants: IUser[];
-	subscriptions: [String];
-	views: Number;
+  name: String;
+  owner: String;
+  team: String[];
+  technologies: String[];
+  likes: Number;
+  description: String;
+  participants: IUser[];
+  subscriptions: [String];
+  views: Number;
+  coreTechnology: String;
+  level: String;
 }
 
 const ProjectModel = new mongoose.Schema(
-	{
-		participants: {
-			type: [mongoose.Schema.Types.ObjectId],
-			ref: "User",
-		},
-		name: String,
-		owner: mongoose.Schema.Types.ObjectId,
-		technologies: [String],
-		likes: Number,
-		description: {
-			type: String,
-			require: true,
-		},
-		type: String,
-		views: {
-			type: Number,
-			default: 0,
-		},
-	},
-	{
-		timestamps: true,
-	},
+  {
+    participants: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "User",
+    },
+    name: String,
+    owner: mongoose.Schema.Types.ObjectId,
+    technologies: [String],
+    likes: Number,
+    level: {
+      type: String,
+      required: true,
+    },
+    coreTechnology: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      require: true,
+    },
+    type: String,
+    views: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 export default mongoose.model<IProject>("Project", ProjectModel);
